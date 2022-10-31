01afbe1ad97e36ea34b650e8134cdb03b99932ad
Erez Weiss <ruggertech@gmail.com>
2022-10-31 21:37:43 +0200

    some test refactoring

--- TriviaGame.UnitTests/GameControllerTests.cs
+++ TriviaGame.UnitTests/GameControllerTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using TriviaGame.Api.Controllers;
 using TriviaGame.Api.entities;
+using TriviaGame.Api.entities.response;
 using TriviaGame.Api.Repositories;
 
 namespace TriviaGame.UnitTests;
@@ -11,6 +12,7 @@ public class GameControllerTests
 {
     private Mock<IGameRepository> m_gameRepositoryStub = new();
     private Mock<IQuestionBucket> m_questionBucketStub = new();
+
     IReadOnlyList<string> m_answers = new List<string>
     {
         "Answer 1",
@@ -35,8 +37,19 @@ public class GameControllerTests
         "Julian Chapman"
     };
 
+    private Mock<ILogger<GameController>> m_loggerStub = new();
+
     [Fact]
     public void CreateGame_WithNonExistentGame_ReturnsNewGameId()
+    {
+        var result = CreateGame();
+
+        // Assert
+        Assert.IsType<string>(result.Id);
+        Assert.NotEmpty(result.Id);
+    }
+
+    private GameResponse CreateGame()
     {
         // Arrange
         m_gameRepositoryStub.Setup(repo => repo.AddGame(It.IsAny<Game>()))
@@ -48,8 +61,8 @@ public class GameControllerTests
                 .Returns(new Question(i, "what is your " + i, m_answers));
         }
 
-        var loggerStub = new Mock<ILogger<GameController>>();
-        var controller = new GameController(loggerStub.Object, m_gameRepositoryStub.Object, m_questionBucketStub.Object);
+        var controller =
+            new GameController(m_loggerStub.Object, m_gameRepositoryStub.Object, m_questionBucketStub.Object);
 
         // Act
         var cr = new GameCreateRequest
@@ -60,37 +73,47 @@ public class GameControllerTests
         };
 
         var result = controller.CreateGame(cr);
-
-        // Assert
-        Assert.IsType<string>(result.Id);
-        Assert.NotEmpty(result.Id);
+        return result;
     }
 
     [Fact]
     public void GetQuestion_WithUserOutOfTheGame_ReturnsError()
     {
+        // Arrange
+        var game = CreateGame();
+
+        var gameWithId = new Game(55, null, null);
+        gameWithId.Id = game.Id;
+        m_gameRepositoryStub.Setup(repo => repo.GetGame(It.IsAny<string>()))
+            .Returns(gameWithId);
+
+        var controller =
+            new GameController(m_loggerStub.Object, m_gameRepositoryStub.Object, m_questionBucketStub.Object);
+
+        var actual = controller.GetQuestion("bla", game.Id);
+        Assert.Equal(new QuestionResponse(1, "dfsf", new List<Answer>()), actual);
     }
-    
+
     [Fact]
     public void GetQuestion_WithUsernameFromGame_ReturnsQuestion()
     {
     }
-    
+
     [Fact]
     public void Answer_WithLessThanSixUsers_ReturnsQuestionPending()
     {
     }
-    
+
     [Fact]
     public void Answer_WithMoreThanElevenUsersOnPendingQuestion_ReturnsQuestionUnresolved()
     {
     }
-    
+
     [Fact]
     public void Answer_WithEightUsersAnswersAreSplitFiftyPercent_ReturnsQuestionPending()
     {
     }
-    
+
     [Fact]
     public void Answer_WithEightUsersWithMajorityVote_ReturnsQuestionResolvedAndCorrectAnswer()
     {
