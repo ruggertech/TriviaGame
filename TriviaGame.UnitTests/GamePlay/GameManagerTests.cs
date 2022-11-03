041cea07e94791ff69f73a0abd5300bc9310d2ee
Erez Weiss <ruggertech@gmail.com>
2022-11-03 19:31:30 +0200

    test awarding points

---
+++ TriviaGame.UnitTests/GamePlay/GameManagerTests.cs
@@ -0,0 +1,91 @@
+using Moq;
+using TriviaGame.Api;
+using TriviaGame.Api.entities;
+using TriviaGame.Api.Repositories;
+
+namespace TriviaGame.UnitTests.GamePlay;
+
+public class GameManagerTests
+{
+    private Mock<IGameRepository> m_gameRepositoryStub = new();
+    private Mock<IQuestionBucket> m_questionBucketStub = new();
+
+    private List<string> m_playerUsernames = new()
+    {
+        "Calvin Malone",
+        "Brynn Norman",
+        "Yvonne Hyde",
+        "Gary Mccall",
+        "Slade O'Neill",
+        "Logan Blair",
+        "Hakeem Graves",
+        "William Durham",
+        "Kiona Mcintyre",
+        "Cleo Sanford",
+        "Declan Mckay",
+        "Julian Chapman",
+        "Bear Walton",
+        "Isobel Horton",
+        "Inaaya Bowen",
+        "Aishah Carson",
+        "Ella-Louise Gregory",
+        "Gavin Mcmahon"
+    };
+
+    IReadOnlyList<string> m_answers = new List<string>
+    {
+        "Answer 1",
+        "Answer 2",
+        "Answer 3",
+        "Answer 4"
+    };
+
+    IReadOnlyList<string> m_questions = new List<string>
+    {
+        "Question 2",
+        "Question 3",
+        "Question 4",
+        "Question 5",
+        "Question 6",
+        "Question 7",
+        "Question 8",
+        "Question 9",
+        "Question 10",
+        "Question 11"
+    };
+
+    [Fact]
+    public void AnswerQuestion_WithAlreadyCorrectAnswer_ReturnsAwardedPointsAsSpecifiedInGame()
+    {
+        // Arrange
+        List<Question> qs = new List<Question>
+        {
+            new(1, "Question 1", m_answers),
+        };
+
+        var game = new Game(1, m_playerUsernames, qs);
+        var gameManager = new GameManager(m_gameRepositoryStub.Object, m_questionBucketStub.Object);
+
+        m_gameRepositoryStub.Setup(repo => repo.GetGame(It.IsAny<string>()))
+            .Returns(game);
+        
+        // 6 votes, by different users
+        qs[0].Votes = new Dictionary<string, int>
+        {
+            { m_playerUsernames[0], 1 },
+            { m_playerUsernames[1], 1 },
+            { m_playerUsernames[2], 1 },
+            { m_playerUsernames[3], 1 },
+            { m_playerUsernames[4], 1 },
+            { m_playerUsernames[5], 3 }
+        };
+        
+        // Act
+        // answer a question with users, and then resolve it
+        var actual = gameManager.Answer(game.Id, qs[0].Id, 
+            m_playerUsernames[0], 1);
+
+        // Assert
+        Assert.Equal(game.PointsPerQuestion, actual.awardedPoints);
+    }
+}
--- TriviaGame.UnitTests/GamePlay/ResolverTests.cs
+++ TriviaGame.UnitTests/GamePlay/ResolverTests.cs
@@ -35,20 +35,6 @@ public class ResolverTests
         "Answer 4"
     };
 
-    IReadOnlyList<string> m_questions = new List<string>
-    {
-        "Question 2",
-        "Question 3",
-        "Question 4",
-        "Question 5",
-        "Question 6",
-        "Question 7",
-        "Question 8",
-        "Question 9",
-        "Question 10",
-        "Question 11"
-    };
-
     [Fact]
     public void ResolveAQuestion_NotEnoughVotesForRightAnswer_ReturnsPending()
     {
@@ -94,14 +80,9 @@ public class ResolverTests
         };
 
         var game = new Game(1, m_playerUsernames, qs);
-
-        // Act
-        // answer a question with users, and then resolve it
-        var totalAnswers = 11;
-        var percentageForCorrect = 0.3;
-        var questionToAnswer = qs[0];
-
+        
         // 6 votes, by different users
+        var questionToAnswer = qs[0];
         questionToAnswer.Votes = new Dictionary<string, int>
         {
             { m_playerUsernames[0], 1 },
@@ -112,6 +93,9 @@ public class ResolverTests
             { m_playerUsernames[5], 3 }
         };
 
+        
+        // Act
+        // answer a question with users, and then resolve it
         IResolver resolver = new Resolver();
         var actual = resolver.Resolve(qs[0], game);
 
