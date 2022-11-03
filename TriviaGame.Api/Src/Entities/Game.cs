bb638465bd761fbc1328c266e57492224fc39ec2
Erez Weiss <ruggertech@gmail.com>
2022-11-03 13:56:05 +0200

    redundant comment

--- TriviaGame.Api/Src/Entities/Game.cs
+++ TriviaGame.Api/Src/Entities/Game.cs
@@ -8,7 +8,6 @@ namespace TriviaGame.Api.entities
         public Game(int pointsPerQuestion, List<string> playersUserNames, List<Question> questionList)
         {
             PointsPerQuestion = pointsPerQuestion;
-            //Players = playersUserNames.Select(uname => new Player(uname)).ToList();
             Players = new Players(playersUserNames);
             Questions = questionList;
         }
