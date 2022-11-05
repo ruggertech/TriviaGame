180844225eb06ab2b9b5406c5cd4a9316cbae40c
Erez Weiss <ruggertech@gmail.com>
2022-11-05 14:23:52 +0200

    format

--- TriviaGame.Api/Src/Entities/Votes.cs
+++ TriviaGame.Api/Src/Entities/Votes.cs
@@ -10,16 +10,18 @@ public class Votes
     private Dictionary<Username, AnswerId> m_Votes;
 
     public int Count => m_Votes.Count;
+    
     public IEnumerable<IGrouping<int, int>> ToGroupedByAnsId => m_Votes.Values.GroupBy(ansId => ansId);
+    
     public bool DidUserVote(string username) => m_Votes.ContainsKey(username);
-
+    
     public HashSet<string> GetPlayerNames(int correctAnswer) => m_Votes
         .Where(kc => kc.Value == correctAnswer)
         .Select(kc => kc.Key)
         .ToHashSet();
-
+    
     public void SetVotes(Dictionary<string, int> dict) => m_Votes = dict;
-
+    
     public void SetSingleVote(Username username, AnswerId answerId)
     {
         // a player could have only one vote per question, if it exists, it will be changed
